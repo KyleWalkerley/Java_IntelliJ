5309439a984ae45c626e0a23b367c04b0c8e8f82
KyleWalkerley <walkerleykyle@gmail.com>
2021-09-08 21:44:11 +0200

    08/09/21

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -36,5 +36,31 @@ public class Main {
 
         System.out.println("Long  Minimum value = " + myMinLongValue);
         System.out.println("Long  Maximum value = " + myMaxLongValue);
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+        short shortTotal = (short) (1000 + 10 *
+                (byteValue + shortValue + intValue));
+
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
     }
 }
