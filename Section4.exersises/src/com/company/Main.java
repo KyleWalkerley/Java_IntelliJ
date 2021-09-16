be1dff76ea987bf4685b651c43d56bb6167d7b35
KyleWalkerley <walkerleykyle@gmail.com>
2021-09-16 11:15:00 +0200

    16/09/21

--- Section4.exersises/src/com/company/Main.java
+++ Section4.exersises/src/com/company/Main.java
@@ -33,15 +33,58 @@ public class Main {
 //        }
 //    }
 
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        if (hourOfDay <0 || hourOfDay >23){
-            return false;
-        }
-        else if ((barking) && (hourOfDay < 8 || hourOfDay > 22)){
-            return true;
-        }
-        return false;
+//    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+//        if (hourOfDay <0 || hourOfDay >23){
+//            return false;
+//        }
+//        else if ((barking) && (hourOfDay < 8 || hourOfDay > 22)){
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    public static boolean isLeapYear(int year) {
+//        if (year >= 1 && year <= 9999) {
+//            if ((year % 4) == 0) {
+//                if ((year % 100) == 0) {
+//                    if ((year % 400) == 0) {
+//                        return true;
+//                    }
+//                } else {
+//                    return true;
+//                }
+//            }
+//
+//        }
+//        return false;
+//    }
+
+//    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+//
+//        int num11 = (int)(num1 * 1000);
+//        int num22 = (int)(num2 * 1000);
+//
+//        if(num11 == num22){
+//            return true;
+//        }
+//        return false;
+//
+//    }
+
+//    public static boolean hasEqualSum(int num1, int num2, int num3){
+//        if ((num1 + num2) == num3){
+//            return true;
+//        }
+//        return false;
+//    }
+
+    public static boolean hasTeen(int a, int b, int c){
+        return ((13 <= a && a <= 19)||
+                (13 <= b && b <= 19)||
+                (13 <= c && c <= 19));
     }
-
-
+    public static boolean isTeen(int a){
+        return (13 <= a && a <= 19);
+    }
 }
+
