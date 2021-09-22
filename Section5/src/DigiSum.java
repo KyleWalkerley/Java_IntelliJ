e1d45edb20b8fd5ca906935973759977266a80db
KyleWalkerley <walkerleykyle@gmail.com>
2021-09-22 10:20:48 +0200

    22/09/21

--- Section5/src/DigiSum.java
+++ Section5/src/DigiSum.java
@@ -16,14 +16,11 @@ public class DigiSum {
 
         int sum = 0;
 
-        // 125 -> 125 / 10 = 12 -> 12 * 10 = 120 -> 125 - 120 = 5
         while (number >0) {
-            // extract the least-significant digit
             int digit = number % 10;
             sum += digit;
 
-            // drop the least-significant digit
-            number /= 10;   // same as number = number / 10;
+            number /= 10;
         }
 
         return sum;
---
+++ Section5/src/SumFirstLastDigit.java
@@ -0,0 +1,19 @@
+public class SumFirstLastDigit {
+    public static int sumFirstAndLastDigit(int number){
+        if (number < 0){
+            return -1;
+        }
+        int lastnumber = number % 10;
+        int firstNumber = 0;
+
+        while (number >0) {
+            int digit = number % 10;
+            if (number < 10){
+                firstNumber = digit;
+            }
+
+            number /= 10;
+        }
+        return firstNumber + lastnumber;
+    }
+}
