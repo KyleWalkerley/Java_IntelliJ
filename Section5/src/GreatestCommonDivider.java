70d5d12501e44d2abed6e8620840a025ba1cfaa9
KyleWalkerley <walkerleykyle@gmail.com>
2021-09-22 11:32:07 +0200

    22/09/21

---
+++ Section5/src/GreatestCommonDivider.java
@@ -0,0 +1,23 @@
+public class GreatestCommonDivider {
+    public static int getGreatestCommonDivisor(int num1,int num2){
+        if (num1 < 10 || num2 < 10){
+            return -1;
+        }
+
+        int div = 0;
+        int big;
+
+        if (num1 > num2){
+            big = num1;
+        }else {
+            big = num2;
+        }
+
+        for (int i = 1; i <= big; i++){
+            if (num1 % i == 0 && num2 % i == 0){
+                div = i;
+            }
+        }
+        return div;
+    }
+}
