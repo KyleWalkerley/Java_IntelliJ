ccf540308d14e346a475c5e6eb4a191fadb6281f
KyleWalkerley <walkerleykyle@gmail.com>
2022-01-18 15:33:34 +0200

    18/01/22

--- Chapter12/src/Vid162/Main.java
+++ Chapter12/src/Vid162/Main.java
@@ -1,4 +1,4 @@
-package Vid159;
+package Vid162;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +19,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",null));
 
         tempExit = new HashMap<String, Integer>();
         tempExit.put("W", 2);
