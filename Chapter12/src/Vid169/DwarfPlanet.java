ae30240f3403b949e15ab8b2ff6ff2637e19d53b
KyleWalkerley <walkerleykyle@gmail.com>
2022-01-19 10:51:02 +0200

    19/01/22

---
+++ Chapter12/src/Vid169/DwarfPlanet.java
@@ -0,0 +1,8 @@
+package Vid169;
+
+public class DwarfPlanet extends HeavenlyBody {
+
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
